70710f93e5cbefaca86ad44e2d4ae9732bbec851
Administrator <administrator@chrisoldwood.com>
2003-06-05 09:22:12 +0000

    Added Socket config dialogs.

--- Win32/SockTrace/Resource.h
+++ Win32/SockTrace/Resource.h
@@ -8,9 +8,11 @@
 #define IDR_APPTOOLBAR                  5
 #define ID_FILE_POPUP                   100
 #define ID_FILE_EXIT                    109
-#define IDD_SOCK_OPTIONS                132
-#define ID_OPTIONS_POPUP                200
-#define ID_OPTIONS_SOCKET               201
+#define IDD_SOCK_CONFIG                 133
+#define ID_VIEW_POPUP                   200
+#define ID_VIEW_CLEAR_TRACE             201
+#define ID_OPTIONS_POPUP                300
+#define ID_OPTIONS_SOCKET               301
 #define ID_HELP_POPUP                   900
 #define ID_HELP_CONTENTS                901
 #define ID_HELP_ABOUT                   902
@@ -18,8 +20,20 @@
 #define IDC_EMAIL                       1084
 #define IDC_VERSION                     1085
 #define IDC_TRACE                       1086
+#define IDC_SOCKETS                     1088
+#define IDC_ADD                         1089
+#define IDC_EDIT                        1090
+#define IDC_SRC_PORT                    1090
+#define IDC_REMOVE                      1091
+#define IDC_DST_PORT                    1091
+#define IDC_DST_HOST                    1092
+#define IDC_SEND_FILE                   1093
+#define IDC_RECV_FILE                   1094
+#define IDC_PROTOCOL                    1095
+#define IDC_CHECK                       1096
 #define IDD_MAIN                        5000
 #define IDD_ABOUT                       5001
+#define IDD_SOCK_OPTIONS                5002
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -27,9 +41,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        133
-#define _APS_NEXT_COMMAND_VALUE         166
-#define _APS_NEXT_CONTROL_VALUE         1088
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         167
+#define _APS_NEXT_CONTROL_VALUE         1097
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
