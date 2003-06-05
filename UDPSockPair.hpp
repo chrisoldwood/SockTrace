3575951e0e9b26f758ed4a763ccd07ad02dca100
Administrator <administrator@chrisoldwood.com>
2003-06-05 09:21:29 +0000

    Reworked to be derived from CSockPair base class.

--- UDPSockPair.hpp
+++ UDPSockPair.hpp
@@ -19,24 +19,20 @@
 *******************************************************************************
 */
 
-class CUDPSockPair
+class CUDPSockPair : public CSockPair
 {
 public:
 	//
 	// Constructors/Destructor.
 	//
-	CUDPSockPair(uint nInstance, CSockConfig* pConfig);
-	~CUDPSockPair();
+	CUDPSockPair(CSockConfig* pConfig, uint nInstance);
+	virtual ~CUDPSockPair();
 	
 	//
 	// Members.
 	//
-	uint			m_nInstance;	// The pair instance ID.
-	CSockConfig*	m_pConfig;		// The socket config.
 	CUDPSvrSocket	m_oInpSocket;	// The client connection.
 	CUDPSvrSocket	m_oOutSocket;	// The server connection.
-	CPath			m_strSendFile;	// Path of log file for data sent.
-	CPath			m_strRecvFile;	// Path of log file for data recieved.
 	in_addr			m_oSrcAddr;		// Client send address.
 	uint			m_nSrcPort;		// Client send port.
 	in_addr			m_oDstAddr;		// Server address.
@@ -50,22 +46,9 @@ public:
 *******************************************************************************
 */
 
-inline CUDPSockPair::CUDPSockPair(uint nInstance, CSockConfig* pConfig)
-	: m_nInstance(nInstance)
-	, m_pConfig(pConfig)
+inline CUDPSockPair::CUDPSockPair(CSockConfig* pConfig, uint nInstance)
+	: CSockPair(pConfig, nInstance)
 {
-	ASSERT(pConfig != NULL);
-
-	// Apply instance number to log file names.
-	m_strSendFile.Format(m_pConfig->m_strSendFile, m_nInstance);
-	m_strRecvFile.Format(m_pConfig->m_strRecvFile, m_nInstance);
-
-	CPath strAppDir = CPath::ApplicationDir();
-
-	// Prepend app folder.
-	m_strSendFile = strAppDir + m_strSendFile;
-	m_strRecvFile = strAppDir + m_strRecvFile;
-
 	// Reset source address and port.
 	m_oSrcAddr.s_addr = INADDR_NONE;
 	m_nSrcPort        = 0;
