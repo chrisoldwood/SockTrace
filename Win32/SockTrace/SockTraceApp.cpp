a626c4104851836e74e8ff8691e2390459ffe9b3
Administrator <administrator@chrisoldwood.com>
2003-06-19 12:19:14 +0000

    Upped version number and copyright date.

--- Win32/SockTrace/SockTraceApp.cpp
+++ Win32/SockTrace/SockTraceApp.cpp
@@ -33,9 +33,9 @@ CSockTraceApp App;
 */
 
 #ifdef _DEBUG
-const char* CSockTraceApp::VERSION      = "v1.0 [Debug]";
+const char* CSockTraceApp::VERSION      = "v1.1 [Debug]";
 #else
-const char* CSockTraceApp::VERSION      = "v1.0";
+const char* CSockTraceApp::VERSION      = "v1.1";
 #endif
 
 const char* CSockTraceApp::INI_FILE_VER  = "1.0";
