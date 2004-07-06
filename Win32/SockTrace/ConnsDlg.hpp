0784379835cc1a271263f1671b46dcccab9009f5
Administrator <administrator@chrisoldwood.com>
2004-07-06 19:57:23 +0000

    Vss2Git

---
+++ Win32/SockTrace/ConnsDlg.hpp
@@ -0,0 +1,65 @@
+/******************************************************************************
+** (C) Chris Oldwood
+**
+** MODULE:		CONNSDLG.HPP
+** COMPONENT:	The Application
+** DESCRIPTION:	The CConnsDlg class declaration.
+**
+*******************************************************************************
+*/
+
+// Check for previous inclusion
+#ifndef CONNSDLG_HPP
+#define CONNSDLG_HPP
+
+/******************************************************************************
+** 
+** .
+**
+*******************************************************************************
+*/
+
+class CConnsDlg : public CDialog
+{
+public:
+	//
+	// Constructors/Destructor.
+	//
+	CConnsDlg();
+	
+protected:
+	//
+	// Members.
+	//
+	
+	//
+	// Controls.
+	//
+	CListView	m_lvSocks;
+
+	// Columns.
+	enum
+	{
+		INSTANCE,
+		PROTOCOL,
+		HOST,
+		PORT,
+		SENT,
+		RECV,
+	};
+
+	//
+	// Message handlers.
+	//
+	virtual void OnInitDialog();
+	virtual bool OnOk();
+};
+
+/******************************************************************************
+**
+** Implementation of inline functions.
+**
+*******************************************************************************
+*/
+
+#endif // CONNSDLG_HPP
